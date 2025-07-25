426c62c416f147e9052421557727a7d5259a9abd
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-07-25 10:20:30 -0500

    HomeScreen finished

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,14 @@
-import { Text, View, Image } from "react-native";
-import posts from "../../../assets/assets/data/posts.json";
-import { formatDistanceToNowStrict } from "date-fns";
+import { Text, View, FlatList } from "react-native";
+import PostListItem from "../../components/PostListItem";
+import posts from "../../../assets/assets/data/posts.json"
+
 export default function HomeScreen() {
-  const post = posts[0];
   return (
     <View>
-      <View style={{ flexDirection: "row", gap: 10 }}>
-        <Image
-          source={{ uri: post.group.image }}
-          style={{ width: 15, height: 15, borderRadius: 10 }}
-        />
-        <Text style={{ fontWeight: "bold" }}>{post.group.name}</Text>
-        <Text style={{ color: "grey" }}>
-          {formatDistanceToNowStrict(new Date(post.created_at))}
-        </Text>
-      </View>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListItem post={item}/>}
+      />
     </View>
   );
 }
