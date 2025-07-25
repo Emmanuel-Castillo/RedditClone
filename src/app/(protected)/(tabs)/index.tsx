f7d9516e9e727b6a4a3967f407a800328aab46a9
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-07-25 12:24:14 -0500

    Detailed Post Screen & Auth

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View, FlatList } from "react-native";
-import PostListItem from "../../components/PostListItem";
-import posts from "../../../assets/assets/data/posts.json"
+import PostListItem from "../../../components/PostListItem";
+import posts from "../../../../assets/assets/data/posts.json"
 
 export default function HomeScreen() {
   return (
---
+++ src/app/(protected)/post/[id].tsx
@@ -0,0 +1,20 @@
+import { useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+import posts from "../../../../assets/assets/data/posts.json"
+import PostListItem from "../../../components/PostListItem";
+
+export default function DetailedPost() {
+    const { id } = useLocalSearchParams()
+
+    const detailedPost = posts.find(post => post.id === id)
+
+    if (!detailedPost) {
+        return <Text>Post Not Found</Text>
+    }
+
+    return (
+        <View>
+            <PostListItem post={detailedPost} isDetailedPost/>
+        </View>
+    )
+}
