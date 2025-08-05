a9a1ffe0dccb4d2fc6f2bb0debf643aa393b4b8a
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-08-05 09:37:26 -0500

    Initial Create Post page

--- src/app/(protected)/(tabs)/create.tsx
+++ src/app/(protected)/(tabs)/create.tsx
@@ -1,9 +1,104 @@
-import { Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useState } from "react";
 
 export default function CreateScreen() {
-    return (
-        <View>
-            <Text>Create Screen</Text>
-        </View>
-    )
+  const [title, setTitle] = useState<string>("");
+  const [bodyText, setBodyText] = useState<string>("");
+
+  const goBack = () => {
+    setTitle("")
+    setBodyText("")
+    router.back()
+  }
+
+  return (
+    <SafeAreaView
+      style={{ backgroundColor: "white", flex: 1, paddingHorizontal: 10 }}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <AntDesign
+          name="close"
+          size={30}
+          color={"black"}
+          onPress={goBack}
+        />
+        <Pressable style={{ marginLeft: "auto" }}>
+          <Text style={styles.postText}>Post</Text>
+        </Pressable>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : undefined}
+        style={{ flex: 1 }}
+      >
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ paddingVertical: 15 }}
+        >
+          {/* COMMUNITY SELECTOR */}
+          <View style={styles.communityContainer}>
+            <Text style={styles.rStyles}>r/</Text>
+            <Text style={{ fontWeight: 600 }}>Select a community</Text>
+          </View>
+
+          {/* INPUTS */}
+          <TextInput
+            multiline
+            placeholder="Title"
+            style={{ fontSize: 20, fontWeight: "bold", paddingVertical: 20 }}
+            value={title}
+            onChangeText={setTitle}
+            scrollEnabled={false}
+          />
+          <TextInput
+            placeholder="Body text (optional)"
+            value={bodyText}
+            onChangeText={setBodyText}
+            multiline
+            scrollEnabled={false}
+          />
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  postText: {
+    color: "white",
+    backgroundColor: "#115BCA",
+    fontWeight: "bold",
+    paddingVertical: 2,
+    paddingHorizontal: 7,
+    borderRadius: 10,
+  },
+  rStyles: {
+    backgroundColor: "black",
+    color: "white",
+    paddingVertical: 1,
+    paddingHorizontal: 5,
+    borderRadius: 10,
+    fontWeight: "bold",
+  },
+  communityContainer: {
+    backgroundColor: "#EDEDED",
+    flexDirection: "row",
+    padding: 10,
+    borderRadius: 20,
+    gap: 5,
+    alignSelf: "flex-start",
+    marginVertical: 10,
+  },
+});
